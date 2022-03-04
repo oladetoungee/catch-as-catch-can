e9cda15662ae781ebc2cd39da73d572dd38f9c0e
oladetoungee <demilad1998@gmail.com>
2022-03-04 11:53:42 +0100

    UI update

--- components/Footerr.vue
+++ components/Footerr.vue
@@ -1,13 +1,58 @@
 <template>
+<div class="footer-container">
     <div class="footer">
-        <img src="~assets/images/Rectangle 2988.png" alt="">
+    <div class="footer-info">
+      <p class="footer-p1">
+        Start doing your local/international <br />
+        delivery with SHiiP
+      </p>
+      <p class="footer-p2">
+        Create a free account in less than 5 minutes <br />
+        and start shipping!
+      </p>
+      <div class="footer-btns">
+        <button class="started">Get started with SHiiP</button>
+        <button><img src="~assets/images/play.svg" alt="" /></button>
+        <p class="footer-p2">See a Live Demo</p>
+      </div>
     </div>
+   
+  </div>
+   <div class="footer-bottom">
+       
+    </div>
+</div>
+  
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
 <style>
-
+.footer {
+  background-color: black;
+  color: #ffffff;
+}
+.footer-info {
+  padding: 50px 10px;
+}
+.footer-p1 {
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 65px;
+}
+.footer-p2 {
+  font-size: 24px;
+  line-height: 27px;
+  margin-top: 15px;
+}
+.footer-btns {
+  display: flex;
+  justify-content: space-between;
+  width: 500px;
+  margin-top: 25px;
+}
+.footer-bottom {
+    background-color: #005FC5;
+    height: 70px;
+}
 </style>
