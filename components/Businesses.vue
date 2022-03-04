f7bb42ac619796f885393053a2e2ac2b6cd44cb0
oladetoungee <demilad1998@gmail.com>
2022-03-04 12:45:00 +0100

    UI update

--- components/Businesses.vue
+++ components/Businesses.vue
@@ -1,13 +1,26 @@
 <template>
-    <div class="businesses">
-
+  <div class="businesses">
+    <div class="business-info">
+        <p class="business-p1">
+            SHiiP works with small, medium and large businesses
+        </p>
+        <p class="business-p2">
+            Run your business smoothly by integrating SHiiP API to your
+websites, e-commerce stores, or marketplace.
+        </p>
+        <p class="business-p2">
+            With SHiiP API, you will be able to
+        </p>
     </div>
+    <div class="platform-info">
+        
+    </div>
+    <div >
+        <button class="learn-btn">Learn more about SHiiP API Integration</button>
+    </div>
+  </div>
 </template>
 <script>
-export default {
-    
-}
+export default {};
 </script>
-<style>
-
-</style>
+<style></style>
